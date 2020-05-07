e904e8936e86f0e8093951de4591f45b2a51692f
azhang@noblr.com <azhang@noblr.com>
2020-05-07 15:02:51 -0700

    2

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,7 +1,8 @@
 d="$(docker ps -a |grep ignite|wc -l)"
-echo $d
 if [ $d -eq 0 ]; then
   docker run -p 10800:10800 --name ignite -d apacheignite/ignite:2.8.0
 fi
-apt install lsof -y
-lsof -i:10800
+d="$(docker ps |grep ignite|wc -l)"
+if [ $d -ne 0 ]; then
+  mvn -B package --file pom.xml
+fi
